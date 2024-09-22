ae4c0bd6b6e786673f97bf02610c56f818ab0ac6
admin <rizkyputri.maharani33@gmail.com>
2024-09-22 16:30:22 +0700

    Initial commit

---
+++ src/main/java/com/enigma/edunity/EdunityApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.edunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enigma/edunity/EdunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.edunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
